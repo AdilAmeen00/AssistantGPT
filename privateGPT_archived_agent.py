9a29468516fe85635ff9aaed3638d20c0b53b206
Ameen001 <81118905+AdilAmeen00@users.noreply.github.com>
2023-09-20 16:38:44 +0530

    Update privateGPT_archived_agent.py

--- privateGPT_archived_agent.py
+++ privateGPT_archived_agent.py
@@ -51,7 +51,8 @@ def run_model(query):
     # with access -> garage-bAInd/Platypus2-70B-instruct
     #   'meta-llama/Llama-2-70b-chat-hf'
     # vicuna -> lmsys/vicuna-13b-delta-v1.1
-    model_id ='lmsys/vicuna-13b-delta-v1.1'
+    # bigscience/bloom-560m
+    model_id ='bigscience/bloom-560m'
     device = f'cuda:{cuda.current_device()}' if cuda.is_available() else 'cpu'
     
     
